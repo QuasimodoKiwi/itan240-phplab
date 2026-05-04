a897e424e848dd512c236242aa184f357e44b90f
Connor Hamilton <clyhamilton@gmail.com>
2026-05-04 14:27:39 -0400

    Moving project to htdocs

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>LaundroHeaven</h1>
+    <p>One day in 202X, LaundroHeaven founder Connor Hamilton decided to open a laundromat here in Reading, Pennsylvania. </p>
 </body>
 </html>
